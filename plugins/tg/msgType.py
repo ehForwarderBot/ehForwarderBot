e4eef5d0b57abe731f85762e3d1163e8fa6155e1
Eana Hufwe <ilove@1a23.com>
2016-06-08 15:21:04 +0800

    20160608-1

--- plugins/tg/msgType.py
+++ plugins/tg/msgType.py
@@ -1,5 +1,15 @@
 def get_msg_type(msg):
     pass
 
+
 class TGMsgType:
-    pass
+    Text = "Text"
+    Audio = "Audio"
+    Document = "Document"
+    Photo = "Photo"
+    Sticker = "Sticker"
+    Video = "Video"
+    Voice = "Voice"
+    Contact = "Contact"
+    Location = "Location"
+    Venue = "Venue"
