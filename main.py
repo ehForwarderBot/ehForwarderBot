a236e4f5889c3beb0f646875cbf3d0b76aba9323
Eana Hufwe <ilove@1a23.com>
2016-12-26 21:40:30 +0800

    Update version tag

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 import argparse
 from daemon import Daemon
 
-__version__ = "1.0 build 20161220"
+__version__ = "1.0 build 20161226"
 
 parser = argparse.ArgumentParser(description="EH Forwarder Bot is an extensible chat tunnel framework which allows "
                                              "users to contact people from other chat platforms, and ultimately "
