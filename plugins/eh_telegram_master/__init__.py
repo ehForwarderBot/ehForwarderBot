d0af3580a99d108e50b1a76c259580aec3c2da0b
Eana Hufwe <ilove@1a23.com>
2017-01-27 14:27:06 +0800

    ETM: Audio conversion fix

--- plugins/eh_telegram_master/__init__.py
+++ plugins/eh_telegram_master/__init__.py
@@ -365,7 +365,7 @@ class TelegramChannel(EFBChannel):
                     pydub.AudioSegment.from_file(msg.file).export("%s.ogg" % msg.path,
                                                                   format="ogg",
                                                                   codec="libopus",
-                                                                  bitrate=65536,
+                                                                  bitrate="65536",
                                                                   parameters=["-vbr", "on", "-compression_level", "10"])
                     ogg_file = open("%s.ogg" % msg.path, 'rb')
                     tg_msg = self.bot.bot.sendVoice(tg_dest, ogg_file, caption=msg_template % msg.text)
