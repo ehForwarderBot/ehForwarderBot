ab97c9fbaad2ae8bc30d63a98bec1fa6fb58dd4d
Eana Hufwe <ilove@1a23.com>
2018-02-01 18:24:25 +0800

    Update requirement to Python 3.6

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import sys
 from setuptools import setup
 
-if sys.version_info < (3, 5):
-    raise Exception("Python 3.5 or higher is required. Your version is %s." % sys.version)
+if sys.version_info < (3, 6):
+    raise Exception("Python 3.6 or higher is required. Your version is %s." % sys.version)
 
 long_description = open('README.rst').read()
 
